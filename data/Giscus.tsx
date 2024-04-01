9c35d6cef0e5a2d896cf136b604eb010cc3abc07
Banana-Master <tjdgh0313@gmail.com>
2024-04-01 20:52:23 +0900

    댓글 lazyloading

--- data/Giscus.tsx
+++ data/Giscus.tsx
@@ -47,9 +47,10 @@ export default function Giscus() {
     scriptElem.setAttribute('data-strict', '0')
     scriptElem.setAttribute('data-reactions-enabled', '1')
     scriptElem.setAttribute('data-emit-metadata', '0')
-    scriptElem.setAttribute('data-input-position', 'bottom')
+    scriptElem.setAttribute('data-input-position', 'top')
     scriptElem.setAttribute('data-theme', theme)
     scriptElem.setAttribute('data-lang', 'ko')
+    scriptElem.setAttribute('data-loading', 'lazy')
 
     ref.current.appendChild(scriptElem)
     console.log(scriptElem)
